816261c1d6489971ee80473aa06905132ba141a6
KW Development <dkiops@yahoo.com>
2021-11-04 16:31:41 -0400

    Update Sort.java

--- Bubble Sort/src/Sort.java
+++ Bubble Sort/src/Sort.java
@@ -27,7 +27,7 @@ public class Sort {
 	
 	public static void main(String[] args) {
 		 Sort ob = new Sort();
-	        int arr[] = {1, 2, 5, 6, 22, 11, 90};
+	        int arr[] = {1,0,5,6,3,2,3,7,9,8,4};
 	        ob.bubbleSort(arr);
 	        System.out.println("Sorted array");
 	        ob.printArray(arr);
